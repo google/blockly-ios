6f22eab70824b4be561248da5a20e308a3457b48
Victor Ng <victor.ng@gmail.com>
2016-03-11 19:16:51 -0500

    Split some code from CodeGenerator into CodeGeneratorService

--- Blockly/Code/Common/CodeGenerator.swift
+++ Blockly/Code/Common/CodeGenerator.swift
@@ -22,9 +22,11 @@ import JavaScriptCore
 /**
  Helper for generating code from a workspace.
 
- This helper is actually a wrapper of a non-UI web version of Blockly, which generates workspace
+ This object is essentially a wrapper of a non-UI web version of Blockly, which generates workspace
  code via JavaScript. For more information on how this works, see:
  https://developers.google.com/blockly/installation/code-generators
+
+ Users should not use this class directly and instead should use `CodeGeneratorService`.
 
  - Note: This object must be instantiated on the main thread, as it internally instantiates a
 `UIWebView` object (which has to be done on the main thread).
@@ -32,89 +34,176 @@ import JavaScriptCore
 @objc(BKYCodeGenerator)
 public class CodeGenerator: NSObject {
 
+  // MARK: - Typealiases
+  public typealias CompletionClosure = (code: String) -> Void
+  public typealias ErrorClosure = (error: String) -> Void
+  /**
+   Tuple defining where to find a local file.
+
+   - Parameter file: The path to a local file
+   - Parameter bundle: The bundle in which to find `file`. If nil is specified, the main bundle
+   should be used.
+  */
+  public typealias BundledFile = (file: String, bundle: NSBundle?)
+
   // MARK: - Static Properties
   /// Internal JS file that is used to communicate between the iOS code and JS code
   private static let CODE_GENERATOR_BRIDGE_JS = "code_generator/code_generator_bridge.js"
 
   // MARK: - Properties
 
+  /// List of core Blockly JS dependencies
+  public let jsCoreDependencies: [BundledFile]
+  /// The name of the JS object that generates code (e.g. 'Blockly.Python')
+  public let jsGeneratorObject: String
+  /// List of block generator JS files (e.g. ['python_compressed.js'])
+  public let jsBlockGenerators: [BundledFile]
+  /// List of JSON files containing block definitions
+  public let jsonBlockDefinitions: [BundledFile]
+
+  // TODO:(#15) Replace UIWebView with WKWebView
   /// The webview used for generating code
   private var webView: UIWebView!
   /// The underlying JSContext of `self.webView`
   private var jsContext: JSContext!
-  /// The name of the JS object that generates code (e.g. 'Blockly.JavaScript')
-  private var jsGeneratorObject: String
-  /// The current request being processed
-  private var currentRequest: Request?
-  /// Operation queue of all pending code generation requests
-  private let requestQueue = NSOperationQueue()
+
+  /// Current workspaceXML being processed
+  private var currentWorkspaceXML: String?
+  /// Callback that is executed when code generation completes successfully
+  private var onCompletion: CompletionClosure?
+  /// Callback that is executed when code generation fails
+  private var onError: ErrorClosure?
 
   // MARK: - Initializers
 
   /**
    Initializer for a code generator.
 
-   - Parameter jsGeneratorObject: Name of the JS object that generates code
-   (e.g. 'Blockly.Python' for generating Python code)
-   - Parameter jsDependencies: Paths to all of the JS dependencies required to generate code. This
+   - Parameter jsCoreDependencies: Paths to core Blockly JS dependencies. This
    list must contain the following files:
      - Blockly engine (eg. 'blockly_compressed.js')
      - Blockly default blocks (eg. 'blocks_compressed.js')
      - A default list of messages (eg. 'msg/js/en.js')
-     - The code generator for the `jsGeneratorObject` (eg. 'python_compressed.js' for generating
-     Python code)
-   - Parameter bundle: The bundle in which to find the files in `jsDependencies`. If this parameter
-   is `nil`, the default main bundle is used.
-  - Throws:
-  `BlocklyError`: Thrown if one or more dependencies could not be loaded.
+   - Parameter jsGeneratorObject: Name of the JS object that generates code
+   (e.g. 'Blockly.Python' for generating Python code)
+   - Parameter jsBlockGenerators: Paths to JS generator files (e.g. 'python_compressed.js' for
+   generating Python code)
+   - Parameter jsonBlockDefinitions: Paths to JSON files containing block definitions
+   - Throws:
+   `BlocklyError`: Thrown if any JS/JSON resource could not be loaded.
    */
-  public init(jsGeneratorObject: String, jsDependencies: [String], bundle: NSBundle? = nil) throws
+  public init(jsCoreDependencies: [BundledFile], jsGeneratorObject: String,
+    jsBlockGenerators: [BundledFile], jsonBlockDefinitions: [BundledFile]) throws
   {
     self.webView = UIWebView()
     self.webView.loadHTMLString("", baseURL: NSURL(string: "about:blank")!)
     self.jsContext =
       webView.valueForKeyPath("documentView.webView.mainFrame.javaScriptContext") as! JSContext
+    self.jsCoreDependencies = jsCoreDependencies
     self.jsGeneratorObject = jsGeneratorObject
+    self.jsBlockGenerators = jsBlockGenerators
+    self.jsonBlockDefinitions = jsonBlockDefinitions
 
     super.init()
 
-    // Only allow one request to execute at a time
-    requestQueue.maxConcurrentOperationCount = 1
-
     // Register a handler if an exception is thrown in the JS code
     jsContext.exceptionHandler = { context, exception in
-      let request = self.currentRequest
-      self.currentRequest = nil
-      request?.completeRequestWithError("JS Exception: \(exception)")
+      let onError = self.onError
+      self.reset()
+      onError?(error: "JS Exception occurred: \(exception)")
     }
 
     // Load our special bridge file
-    try loadJSFile(CodeGenerator.CODE_GENERATOR_BRIDGE_JS,
-      bundle: NSBundle(forClass: CodeGenerator.self))
+    try loadJSFile((file: CodeGenerator.CODE_GENERATOR_BRIDGE_JS,
+      bundle: NSBundle(forClass: CodeGenerator.self)))
 
-    // Load dependencies
-    for file in jsDependencies {
-      try loadJSFile(file, bundle: bundle)
+    // Load JS dependencies
+    for bundledFile in jsCoreDependencies {
+      try loadJSFile(bundledFile)
+    }
+
+    // Load block generators
+    for bundledFile in jsBlockGenerators {
+      try loadJSFile(bundledFile)
+    }
+
+    // Load block definitions
+    for bundledFile in jsonBlockDefinitions {
+      try importBlockDefinitionsFromFile(bundledFile)
     }
   }
 
   deinit {
-    cancelAllRequests()
+    self.webView.stopLoading()
   }
 
   // MARK: - Public
 
   /**
+   Generates code for workspace XML.
+   
+   - Note: Only one request may be made at a time. If another request is still pending, this method
+   will immediately execute the `error` block.
+
+   - Parameter workspaceXML: The workspace XML
+   */
+  public func generateCodeForWorkspaceXML(
+    workspaceXML: String, completion: CompletionClosure, error: ErrorClosure)
+  {
+    // Generate the code.
+    // Note: If this request is called on a background thread, it can sometimes cause an
+    // EXC_BAD_ACCESS to be thrown on a WebCore thread. Therefore, we have chosen to execute this
+    // on the main thread as it doesn't seem to crash out. Luckily, generating code for a workspace
+    // is a fast operation, so this *shouldn't* be a big performance hit.
+    // TODO:(#14) Investigate the performance of this on a large workspace and look into using
+    // Web Workers in the JS code instead.
+    dispatch_async(dispatch_get_main_queue()) {
+      if self.currentWorkspaceXML != nil {
+        error(error: "Another code generation request is still being processed. " +
+          "Maybe you should try using `CodeGeneratorService` instead.")
+        return
+      }
+
+      self.currentWorkspaceXML = workspaceXML
+      self.onCompletion = completion
+      self.onError = error
+      
+      let generator = self.jsContext.evaluateScript(self.jsGeneratorObject)
+      let method = self.jsContext.evaluateScript("CodeGeneratorBridge.generateCodeForWorkspace")
+      let returnValue = method.callWithArguments([workspaceXML, generator])
+
+      if let code = returnValue.toString() {
+        // Success!
+        let onCompletion = self.onCompletion
+        self.reset()
+        onCompletion?(code: code)
+      } else {
+        // Fail :(
+        let onError = self.onError
+        self.reset()
+        onError?(error: "Could not convert return value into a String.")
+      }
+    }
+  }
+
+  // MARK: - Private
+
+  private func reset() {
+    self.currentWorkspaceXML = nil
+    self.onCompletion = nil
+    self.onError = nil
+  }
+
+  /**
    Imports block definitions from a given JSON file.
 
-   - Parameter file: The path to the file containing the JSON block definitions.
-   - Parameter bundle: The bundle in which to find `file`. If this parameter is nil, the default
-   main bundle is used.
+   - Parameter bundledFile: The path to the JSON file.
    - Throws:
    `BlocklyError`: Thrown if there was an error loading the JSON file.
    */
-  public func importBlockDefinitionsFromFile(file: String, bundle: NSBundle? = nil) throws {
-    let fromBundle = bundle ?? NSBundle.mainBundle()
+  private func importBlockDefinitionsFromFile(bundledFile: BundledFile) throws {
+    let fromBundle = bundledFile.bundle ?? NSBundle.mainBundle()
+    let file = bundledFile.file
     guard let path = fromBundle.pathForResource(file, ofType: nil) else {
       throw BlocklyError(.FileNotFound, "JSON file could not be found ('\(file)').")
     }
@@ -129,82 +218,15 @@ public class CodeGenerator: NSObject {
   }
 
   /**
-   Imports custom block generators from a given JavaScript file.
+   Loads a JS file into `self.webView`.
 
-   - Parameter file: The path to the JavaScript file containing the block generators.
-   - Parameter bundle: The bundle in which to find `file`. If this parameter is nil, the default
-   main bundle is used.
+   - Parameter bundledFile: The path to the JS file.
    - Throws:
-   `BlocklyError`: Thrown if there was an error importing the JavaScript file.
+   `BlocklyError`: Thrown if there was an error loading the JS file.
    */
-  public func importBlockGeneratorsFromFile(file: String, bundle: NSBundle? = nil) throws {
-    try loadJSFile(file, bundle: bundle)
-  }
-
-  /**
-   Sends a request to generate code.
-
-   If the request completes successfully, the request's `onCompletion` block is executed.
-   If the request fails, the request's `onError` block is executed.
-
-   - Parameter request: The request
-   */
-  public func generateCodeForRequest(request: Request) {
-    request.codeGenerator = self
-    requestQueue.addOperation(request)
-  }
-
-  /**
-   Cancels code generation for a given request.
-   */
-  public func cancelRequest(request: Request) {
-    request.cancel()
-  }
-
-  /**
-   Cancels all pending code generation requests.
-   */
-  public func cancelAllRequests() {
-    requestQueue.cancelAllOperations()
-  }
-
-  // MARK: - Private
-
-  /**
-   Generates code for a given request.
-
-   - Parameter request: The request
-   */
-  private func executeCodeGenerationForRequest(request: Request) {
-    self.currentRequest = request
-
-    // Generate the code.
-    // Note: If this request is called on a background thread, it can sometimes cause an
-    // EXC_BAD_ACCESS to be thrown on a WebCore thread. Therefore, we have chosen to execute this
-    // on the main thread as it doesn't seem to crash out. Luckily, generating code for a workspace
-    // is a fast operation, so this *shouldn't* be a big performance hit.
-    // TODO:(#14) Investigate the performance of this on a large workspace.
-    dispatch_async(dispatch_get_main_queue()) {
-      let generator = self.jsContext.evaluateScript(self.jsGeneratorObject)
-      let method = self.jsContext.evaluateScript("CodeGeneratorBridge.generateCodeForWorkspace")
-      let returnValue = method.callWithArguments([self.currentRequest!.workspaceXML, generator])
-
-      if let code = returnValue.toString() {
-        // Success!
-        let request = self.currentRequest
-        self.currentRequest = nil
-        request?.completeRequestWithCode(code)
-      } else {
-        // Fail :(
-        let request = self.currentRequest
-        self.currentRequest = nil
-        request?.completeRequestWithError("Could not convert return value into a String.")
-      }
-    }
-  }
-
-  private func loadJSFile(file: String, bundle: NSBundle? = nil) throws {
-    let fromBundle = bundle ?? NSBundle.mainBundle()
+  private func loadJSFile(bundledFile: BundledFile) throws {
+    let fromBundle = bundledFile.bundle ?? NSBundle.mainBundle()
+    let file = bundledFile.file
     if let path = fromBundle.pathForResource(file, ofType: nil) {
       do {
         let string = try String(contentsOfFile: path, encoding: NSUTF8StringEncoding)
@@ -214,123 +236,6 @@ public class CodeGenerator: NSObject {
       }
     } else {
       throw BlocklyError(.FileNotFound, "JS file could not be found ('\(file)').")
-    }
-  }
-}
-
-// MARK: - CodeGenerator.Request Class
-
-extension CodeGenerator {
-  /**
-   Request object for generating code for a workspace.
-   */
-  public class Request: NSOperation {
-    /// MARK: - Type Aliases
-    public typealias CompletionClosure = (code: String) -> Void
-    public typealias ErrorClosure = (error: String) -> Void
-
-    // MARK: - Properties
-    /// The workspace XML to use when generating code
-    public let workspaceXML: String
-    /// Callback that is executed when code generation completes successfully
-    public var onCompletion: CompletionClosure?
-    /// Callback that is executed when code generation fails
-    public var onError: ErrorClosure?
-    /// The code generator used for executing this request.
-    private weak var codeGenerator: CodeGenerator?
-
-    // MARK: - Initializers
-
-    public init(
-      workspaceXML: String, completion: CompletionClosure? = nil, error: ErrorClosure? = nil)
-    {
-      self.workspaceXML = workspaceXML
-      self.onCompletion = completion
-      self.onError = error
-    }
-
-    public convenience init(
-      workspace: Workspace, completion: CompletionClosure? = nil, error: ErrorClosure? = nil) throws
-    {
-      self.init(workspaceXML: try workspace.toXML().xmlString, completion: completion, error: error)
-    }
-
-    // MARK: - Super
-
-    private var _executing: Bool = false
-    public override var executing: Bool {
-      get { return _executing }
-      set {
-        if _executing == newValue {
-          return
-        }
-        willChangeValueForKey("isExecuting")
-        _executing = newValue
-        didChangeValueForKey("isExecuting")
-      }
-    }
-
-    private var _finished: Bool = false;
-    public override var finished: Bool {
-      get { return _finished }
-      set {
-        if _finished == newValue {
-          return
-        }
-        willChangeValueForKey("isFinished")
-        _finished = newValue
-        didChangeValueForKey("isFinished")
-      }
-    }
-
-    public override func start() {
-      if self.cancelled {
-        return
-      }
-
-      self.executing = true
-
-      codeGenerator?.executeCodeGenerationForRequest(self)
-    }
-
-    public override func cancel() {
-      self.onCompletion = nil
-      self.onError = nil
-      super.cancel()
-    }
-
-    // MARK: - Private
-
-    /**
-     Called by `CodeGenerator` if the code generation was completely successfully.
-
-     - Parameter code: The code that was generated
-     */
-    private func completeRequestWithCode(code: String) {
-      if self.cancelled {
-        return
-      }
-
-      dispatch_async(dispatch_get_main_queue()) {
-        self.onCompletion?(code: code)
-      }
-      self.finished = true
-    }
-
-    /**
-     Called by `CodeGenerator` if the code generation failed.
-
-     - Parameter error: An error describing why the code generation failed
-     */
-    private func completeRequestWithError(error: String) {
-      if self.cancelled {
-        return
-      }
-
-      dispatch_async(dispatch_get_main_queue()) {
-        self.onError?(error: error)
-      }
-      self.finished = true
     }
   }
 }
---
+++ Blockly/Code/Common/CodeGeneratorService.swift
@@ -0,0 +1,260 @@
+/*
+* Copyright 2016 Google Inc. All Rights Reserved.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import Foundation
+import AEXML
+
+/**
+ Service for generating code from a workspace.
+
+ Internally, this object uses instances of `CodeGenerator` to execute requests. Please see
+ `CodeGenerator` for more information.
+ */
+@objc(BKYCodeGeneratorService)
+public class CodeGeneratorService: NSObject {
+
+  // MARK: - Properties
+
+  /// List of core Blockly JS dependencies
+  private let jsCoreDependencies: [CodeGenerator.BundledFile]
+  /// Current code generator
+  private var codeGenerator: CodeGenerator?
+  /// Operation queue of all pending code generation requests
+  private let requestQueue = NSOperationQueue()
+
+  // MARK: - Initializers
+
+  /**
+   Creates a code generator service.
+
+   - Parameter jsCoreDependencies: Paths to core Blockly JS dependencies. These core dependencies
+   will be used when an internal `CodeGenerator` instance is created. This list must contain the
+   following files:
+     - Blockly engine (eg. 'blockly_compressed.js')
+     - Blockly default blocks (eg. 'blocks_compressed.js')
+     - A default list of messages (eg. 'msg/js/en.js')
+   */
+  public init(jsCoreDependencies: [CodeGenerator.BundledFile]) {
+    self.jsCoreDependencies = jsCoreDependencies
+    super.init()
+
+    // Only allow one request to execute at a time
+    requestQueue.maxConcurrentOperationCount = 1
+  }
+
+  deinit {
+    cancelAllRequests()
+  }
+
+  /**
+   Sends a request to generate code.
+
+   If the request completes successfully, the request's `onCompletion` block is executed.
+   If the request fails, the request's `onError` block is executed.
+
+   - Parameter request: The request
+   */
+  public func generateCodeForRequest(request: Request) {
+    request.codeGeneratorService = self
+    requestQueue.addOperation(request)
+  }
+
+  /**
+   Cancels code generation for a given request.
+   */
+  public func cancelRequest(request: Request) {
+    request.cancel()
+  }
+
+  /**
+   Cancels all pending code generation requests.
+   */
+  public func cancelAllRequests() {
+    requestQueue.cancelAllOperations()
+  }
+
+  // MARK: - Private
+
+  private func executeRequest(request: Request) {
+    if let codeGenerator = self.codeGenerator where
+      (compareLists(codeGenerator.jsonBlockDefinitions, request.jsonBlockDefinitions) &&
+      compareLists(codeGenerator.jsBlockGenerators, request.jsBlockGenerators) &&
+      codeGenerator.jsGeneratorObject == request.jsGeneratorObject)
+    {
+      // No JS/JSON files have changed since the last request. Use the existing code generator.
+      codeGenerator.generateCodeForWorkspaceXML(request.workspaceXML,
+        completion: request.completeRequestWithCode,
+        error: request.completeRequestWithError)
+    } else {
+      // Use a new code generator (`CodeGenerator` must be instantiated on the main thread)
+      dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        do {
+          self.codeGenerator = try CodeGenerator(
+            jsCoreDependencies: self.jsCoreDependencies,
+            jsGeneratorObject: request.jsGeneratorObject,
+            jsBlockGenerators: request.jsBlockGenerators,
+            jsonBlockDefinitions: request.jsonBlockDefinitions)
+
+          self.codeGenerator!.generateCodeForWorkspaceXML(request.workspaceXML,
+            completion: request.completeRequestWithCode,
+            error: request.completeRequestWithError)
+        } catch let error as NSError {
+          request.completeRequestWithError("Could not create CodeGenerator instance:\n\(error)")
+        }
+      })
+    }
+  }
+
+  private func compareLists(
+    list1: [CodeGenerator.BundledFile], _ list2: [CodeGenerator.BundledFile]) -> Bool
+  {
+    if list1.count != list2.count {
+      return false
+    }
+    var mutableList1 = list1
+    for path2 in list2 {
+      if let index = mutableList1.indexOf({ $0.file == path2.file && $0.bundle == path2.bundle }) {
+        mutableList1.removeAtIndex(index)
+      } else {
+        return false
+      }
+    }
+    return true
+  }
+}
+
+// MARK: - CodeGeneratorService.Request Class
+
+extension CodeGeneratorService {
+  /**
+   Request object for generating code for a workspace.
+   */
+  public class Request: NSOperation {
+    // MARK: - Typealiases
+    public typealias CompletionClosure = (code: String) -> Void
+    public typealias ErrorClosure = (error: String) -> Void
+
+    // MARK: - Properties
+    /// The workspace XML to use when generating code
+    public let workspaceXML: String
+    /// The name of the JS object that generates code (e.g. 'Blockly.Python')
+    public let jsGeneratorObject: String
+    /// List of block generator JS files (e.g. ['python_compressed.js'])
+    public let jsBlockGenerators: [CodeGenerator.BundledFile]
+    /// List of JSON files containing block definitions
+    public let jsonBlockDefinitions: [CodeGenerator.BundledFile]
+    /// Callback that is executed when code generation completes successfully
+    public var onCompletion: CompletionClosure?
+    /// Callback that is executed when code generation fails
+    public var onError: ErrorClosure?
+    /// The code generator service used for executing this request.
+    private weak var codeGeneratorService: CodeGeneratorService?
+
+    // MARK: - Initializers
+
+    public init(workspaceXML: String,
+      jsGeneratorObject: String,
+      jsBlockGenerators: [CodeGenerator.BundledFile],
+      jsonBlockDefinitions: [CodeGenerator.BundledFile],
+      completion: CompletionClosure? = nil, error: ErrorClosure? = nil)
+    {
+      self.workspaceXML = workspaceXML
+      self.jsGeneratorObject = jsGeneratorObject
+      self.jsBlockGenerators = jsBlockGenerators
+      self.jsonBlockDefinitions = jsonBlockDefinitions
+      self.onCompletion = completion
+      self.onError = error
+    }
+
+    public convenience init(workspace: Workspace,
+      jsGeneratorObject: String,
+      jsBlockGenerators: [CodeGenerator.BundledFile],
+      jsonBlockDefinitions: [CodeGenerator.BundledFile],
+      completion: CompletionClosure? = nil, error: ErrorClosure? = nil) throws
+    {
+      self.init(workspaceXML: try workspace.toXML().xmlString,
+        jsGeneratorObject: jsGeneratorObject, jsBlockGenerators: jsBlockGenerators,
+        jsonBlockDefinitions: jsonBlockDefinitions, completion: completion, error: error)
+    }
+
+    // MARK: - Super
+
+    private var _executing: Bool = false
+    public override var executing: Bool {
+      get { return _executing }
+      set {
+        if _executing == newValue {
+          return
+        }
+        willChangeValueForKey("isExecuting")
+        _executing = newValue
+        didChangeValueForKey("isExecuting")
+      }
+    }
+
+    private var _finished: Bool = false;
+    public override var finished: Bool {
+      get { return _finished }
+      set {
+        if _finished == newValue {
+          return
+        }
+        willChangeValueForKey("isFinished")
+        _finished = newValue
+        didChangeValueForKey("isFinished")
+      }
+    }
+
+    public override func start() {
+      if self.cancelled {
+        finishOperation()
+        return
+      }
+      self.executing = true
+
+      // Execute the request. The operation will eventually execute:
+      // completeRequestWithCode(...) or
+      // completeRequestWithError(...)
+      codeGeneratorService?.executeRequest(self)
+    }
+
+    public override func cancel() {
+      self.onCompletion = nil
+      self.onError = nil
+      super.cancel()
+    }
+
+    // MARK: - Private
+
+    private func completeRequestWithCode(code: String) {
+      if !self.cancelled {
+        self.onCompletion?(code: code)
+      }
+      finishOperation()
+    }
+
+    private func completeRequestWithError(error: String) {
+      if !self.cancelled {
+        self.onError?(error: error)
+      }
+      finishOperation()
+    }
+
+    private func finishOperation() {
+      self.executing = false
+      self.finished = true
+    }
+  }
+}
