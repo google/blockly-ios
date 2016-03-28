b5656bded1a9ff291324f54cdc71891d33d2a5c5
Victor Ng <vicng@google.com>
2016-03-28 13:00:26 -0700

    Changed ObjectPool to use a cache instead of a dictionary

--- Blockly/Code/Common/ObjectPool.swift
+++ Blockly/Code/Common/ObjectPool.swift
@@ -16,22 +16,22 @@
 import Foundation
 
 /**
-Defines a protocol for objects that can be recycled for re-use.
-*/
+ Defines a protocol for objects that can be recycled for re-use.
+ */
 @objc(BKYRecyclable)
 public protocol Recyclable: class {
   /**
-  Instantiates a new instance of the recyclable class.
-  */
+   Instantiates a new instance of the recyclable class.
+   */
   init()
 
   /**
-  Reset the object to a fresh state, releasing and recycling any resources associated with this
-  object.
+   Reset the object to a fresh state, releasing and recycling any resources associated with this
+   object.
 
-  - Note: This should not be called directly by clients. To recycle the object, it should be done
-  via `objectPool.recycleObject(object)`.
-  */
+   - Note: This should not be called directly by clients. To recycle the object, it should be done
+   via `objectPool.recycleObject(object)`.
+   */
   func prepareForReuse()
 }
 
@@ -45,8 +45,8 @@ public class ObjectPool: NSObject {
   // MARK: - Properties
 
   /// Keeps track of all recycled objects. Objects of the same type are keyed by their class name
-  /// and stored in an array.
-  private var _recycledObjects = [String: Array<Recyclable>]()
+  /// and stored in a cache.
+  private let _recycledObjects = NSCache()
 
   // MARK: - Public
 
@@ -65,23 +65,23 @@ public class ObjectPool: NSObject {
   }
 
   /**
-  If a recycled object is available for re-use, that object is returned.
+   If a recycled object is available for re-use, that object is returned.
 
-  If not, a new object of the given `Recyclable.Type` is instantiated.
+   If not, a new object of the given `Recyclable.Type` is instantiated.
 
-  - Parameter type: The type of `Recyclable` object to retrieve.
-  - Note: Objects obtained through this method should be recycled through `recycleObject(:)`.
-  - Warning: This method should only be called by Objective-C code. Swift code should use
-  `objectForType(type:)` instead.
-  - Returns: An object of the given type.
-  */
+   - Parameter type: The type of `Recyclable` object to retrieve.
+   - Note: Objects obtained through this method should be recycled through `recycleObject(:)`.
+   - Warning: This method should only be called by Objective-C code. Swift code should use
+   `objectForType(type:)` instead.
+   - Returns: An object of the given type.
+   */
   public func recyclableObjectForType(type: Recyclable.Type) -> Recyclable {
     let className = String(type)
 
-    // Note: _recycledObjects[className] is purposely not assigned to a variable here since that
-    // means we will end up modifying a copy of the array, and not the original array
-    if (_recycledObjects[className]?.count ?? 0) > 0 {
-      return _recycledObjects[className]!.removeLast()
+    if let list = _recycledObjects.objectForKey(className) as? NSMutableArray where list.count > 0 {
+      let recycledObject = list.lastObject as! Recyclable
+      list.removeLastObject()
+      return recycledObject
     }
 
     // Couldn't find a recycled object, create a new instance
@@ -89,28 +89,33 @@ public class ObjectPool: NSObject {
   }
 
   /**
-  Calls `prepareForReuse()` on the object and stores it for re-use later.
+   Calls `prepareForReuse()` on the object and stores it for re-use later.
 
-  - Parameter object: The object to recycle.
-  - Note: Objects recycled through this method should be obtained through `objectForType(:)` or
-  `recyclableObjectForType(:)`.
-  */
+   - Parameter object: The object to recycle.
+   - Note: Objects recycled through this method should be obtained through `objectForType(:)` or
+   `recyclableObjectForType(:)`.
+   */
   public func recycleObject(object: Recyclable) {
     // Prepare the object for re-use
     object.prepareForReuse()
 
     let className = String(object.dynamicType)
 
-    // Note: _recycledObjects[className] is purposely not assigned to a variable here since that
-    // means we will end up modifying a copy of the array, and not the original array
-    if _recycledObjects[className] != nil {
+    if let list = _recycledObjects.objectForKey(className) as? NSMutableArray {
       // Append to the array
-      _recycledObjects[className]!.append(object)
+      list.addObject(object)
     } else {
-      // Create a new array
-      _recycledObjects[className] = [object]
+      // Create a new array. Because the array will be constantly changing, it is faster to use an
+      // NSMutableArray instead of an Array<Recyclable> here, as any changes to the array will be
+      // made by reference instead of by value.
+      _recycledObjects.setObject(NSMutableArray(array: [object]), forKey: className)
     }
   }
 
-  // TODO:(#42) Add a method to prune the pool if it gets too big.
+  /**
+   Removes all recycled objects from memory.
+   */
+  public func removeAllRecycledObjects() {
+    _recycledObjects.removeAllObjects()
+  }
 }
