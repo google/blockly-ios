a8f00a47678a00b62343dbc200d95561fd1edccd
Victor Ng <vicng@google.com>
2016-04-12 20:21:45 -0700

    - Updated config to use a getter/setter based system. - Fixed WorkspaceFlowLayout so it doesn't rely on default layout config properties

---
+++ Blockly/Code/Layout/LayoutConfig.swift
@@ -0,0 +1,296 @@
+/*
+* Copyright 2016 Google Inc. All Rights Reserved.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import Foundation
+
+/**
+ UI configuration for all layout elements.
+ */
+@objc(BKYLayoutConfig)
+public class LayoutConfig: NSObject {
+  // MARK: - Structs
+
+  /**
+   Struct for representing a unit value in both the Workspace coordinate system and UIView
+   coordinate system.
+   */
+  public struct Unit {
+    /// The unit value specified in the Workspace coordinate system
+    public var workspaceUnit: CGFloat
+    /// The unit value specified in the UIView coordinate system
+    public var viewUnit: CGFloat
+
+    public init(_ workspaceUnit: CGFloat, _ viewUnit: CGFloat? = nil) {
+      self.workspaceUnit = workspaceUnit
+      self.viewUnit = viewUnit ?? workspaceUnit
+    }
+  }
+
+  /**
+   Struct for representing a Size value (i.e. width/height) in both the Workspace coordinate
+   system and UIView coordinate system.
+   */
+  public struct Size {
+    /// The size value specified in the Workspace coordinate system
+    public var workspaceSize: WorkspaceSize
+    /// The size value specified in the UIView coordinate system
+    public var viewSize: CGSize
+
+    public init(_ workspaceSize: WorkspaceSize, _ viewSize: CGSize? = nil) {
+      self.workspaceSize = workspaceSize
+      self.viewSize = viewSize ?? workspaceSize
+    }
+  }
+
+  // MARK: - Type Alias
+
+  /// Type alias defining the property key.
+  /// To create a new one, call `LayoutConfig.newPropertyKey()`, rather than instantiating
+  /// the underlying type directly.
+  public typealias PropertyKey = Int
+
+  // MARK: - Static Properties
+
+  /// Total number of `PropertyKey` values that have been created via `newPropertyKey()`.
+  private static var NUMBER_OF_PROPERTY_KEYS = 0
+
+  /// [`Unit`] Horizontal padding around inline elements (such as fields or inputs)
+  public static let InlineXPadding = LayoutConfig.newPropertyKey()
+
+  /// [`Unit`] Vertical padding around inline elements (such as fields or inputs)
+  public static let InlineYPadding = LayoutConfig.newPropertyKey()
+
+  /// [`Unit`] Horizontal space between blocks for `WorkspaceFlowLayout`
+  public static let WorkspaceFlowXSeparatorSpace = LayoutConfig.newPropertyKey()
+
+  /// [`Unit`] Vertical space between blocks for for `WorkspaceFlowLayout`
+  public static let WorkspaceFlowYSeparatorSpace = LayoutConfig.newPropertyKey()
+
+  /// [`Unit`] Minimum height of field rows
+  public static let FieldMinimumHeight = LayoutConfig.newPropertyKey()
+
+  /// [`Unit`] If necessary, the rounded corner radius of a field
+  public static let FieldCornerRadius = LayoutConfig.newPropertyKey()
+
+  /// [`Unit`] If necessary, the line stroke width of a field
+  public static let FieldLineWidth = LayoutConfig.newPropertyKey()
+
+  /// [`Unit`] The border width to use when rendering the `FieldColour` button
+  public static let FieldColourButtonBorderWidth = LayoutConfig.newPropertyKey()
+
+  /// [`Size`] The button size to use when rendering a `FieldColour`
+  public static let FieldColourButtonSize = LayoutConfig.newPropertyKey()
+
+  /// [`UIColor`] The color to use for the `FieldCheckboxView` switch's "onTintColor". A value of
+  /// `nil` means that the system default should be used.
+  public static let FieldCheckboxSwitchOnTintColor = LayoutConfig.newPropertyKey()
+
+  /// [`UIColor`] The color to use for the `FieldCheckboxView` switch's "tintColor". A value of
+  /// `nil` means that the system default should be used.
+  public static let FieldCheckboxSwitchTintColor = LayoutConfig.newPropertyKey()
+
+  // MARK: - Properties
+
+  // NOTE: Separate dictionaries were created for each type of value as casting specific values
+  // from a Dictionary<PropertyKey, Any> is a big performance hit.
+
+  /// Dictionary mapping property keys to `Unit` values
+  private var _units = Dictionary<PropertyKey, Unit>()
+
+  /// Dictionary mapping property keys to `Size` values
+  private var _sizes = Dictionary<PropertyKey, Size>()
+
+  /// Dictionary mapping property keys to `UIColor` values
+  private var _colors = Dictionary<PropertyKey, UIColor>()
+
+  // MARK: - Initializers
+
+  public override init() {
+    super.init()
+
+    // Set default values for base config keys
+    setUnit(Unit(10), forKey: LayoutConfig.InlineXPadding)
+    setUnit(Unit(5), forKey: LayoutConfig.InlineYPadding)
+    setUnit(Unit(10), forKey: LayoutConfig.WorkspaceFlowXSeparatorSpace)
+    setUnit(Unit(10), forKey: LayoutConfig.WorkspaceFlowYSeparatorSpace)
+
+    setUnit(Unit(18), forKey: LayoutConfig.FieldMinimumHeight)
+    setUnit(Unit(5), forKey: LayoutConfig.FieldCornerRadius)
+    setUnit(Unit(1), forKey: LayoutConfig.FieldLineWidth)
+    setSize(Size(WorkspaceSizeMake(44, 44)), forKey: LayoutConfig.FieldColourButtonSize)
+    setUnit(Unit(2), forKey: LayoutConfig.FieldColourButtonBorderWidth)
+
+    // Use the default system colors by setting these config values to nil
+    setColor(nil, forKey: LayoutConfig.FieldCheckboxSwitchOnTintColor)
+    setColor(nil, forKey: LayoutConfig.FieldCheckboxSwitchTintColor)
+  }
+
+  // MARK: - Public
+
+  /**
+  Creates a new `PropertyKey`.
+  */
+  public static func newPropertyKey() -> PropertyKey {
+    let key = NUMBER_OF_PROPERTY_KEYS
+    NUMBER_OF_PROPERTY_KEYS += 1
+    return key
+  }
+
+  /**
+   Maps a `Unit` value to a specific `PropertyKey`.
+
+   - Parameter unit: The `Unit` value
+   - Parameter key: The `PropertyKey` (e.g. `LayoutConfig.InlineXPadding`)
+   - Returns: The `unit` that was set.
+   */
+  public func setUnit(unit: Unit?, forKey key: PropertyKey) -> Unit? {
+    _units[key] = unit
+    return unit
+  }
+
+  /**
+   Returns the `Unit` value that is mapped to a specific `PropertyKey`.
+
+   - Parameter key: The `PropertyKey` (e.g. `LayoutConfig.InlineXPadding`)
+   - Parameter defaultValue: [Optional] If no value was found for `key`, this value is automatically
+   assigned to `key` and used instead.
+   - Returns: The mapped `Unit` value.
+   */
+  @inline(__always)
+  public func unitFor(key: PropertyKey, defaultValue: Unit = Unit(0, 0)) -> Unit {
+    return _units[key] ?? setUnit(defaultValue, forKey: key)!
+  }
+
+  /**
+   Returns the `viewUnit` of the `Unit` value that is mapped to a specific `PropertyKey`.
+
+   - Parameter key: The `PropertyKey` (e.g. `LayoutConfig.InlineXPadding`)
+   - Parameter defaultValue: [Optional] If no value was found for `key`, this value is automatically
+   assigned to `key` and used instead.
+   - Returns: The `viewUnit` of the mapped `Unit` value.
+   */
+  @inline(__always)
+  public func viewUnitFor(key: PropertyKey, defaultValue: Unit = Unit(0)) -> CGFloat {
+    return unitFor(key, defaultValue: defaultValue).viewUnit
+  }
+
+  /**
+   Returns the `workspaceUnit` of the `Unit` value that is mapped to a specific `PropertyKey`.
+
+   - Parameter key: The `PropertyKey` (e.g. `LayoutConfig.InlineXPadding`)
+   - Parameter defaultValue: [Optional] If no value was found for `key`, this value is automatically
+   assigned to `key` and used instead.
+   - Returns: The `workspaceUnit` of the mapped `Unit` value.
+   */
+  @inline(__always)
+  public func workspaceUnitFor(key: PropertyKey, defaultValue: Unit = Unit(0)) -> CGFloat {
+    return unitFor(key, defaultValue: defaultValue).workspaceUnit
+  }
+
+  /**
+   Maps a `Size` value to a specific `PropertyKey`.
+
+   - Parameter size: The `Size` value
+   - Parameter key: The `PropertyKey` (e.g. `LayoutConfig.FieldColourButtonSize`)
+   - Returns: The `size` that was set.
+   */
+  public func setSize(size: Size?, forKey key: PropertyKey) -> Size? {
+    _sizes[key] = size
+    return size
+  }
+
+  /**
+   Returns the `Size` value that is mapped to a specific `PropertyKey`.
+
+   - Parameter key: The `PropertyKey` (e.g. `LayoutConfig.FieldColourButtonSize`)
+   - Parameter defaultValue: [Optional] If no `Size` was found for `key`, this value is
+   automatically assigned to `key` and used instead.
+   - Returns: The mapped `Size` value.
+   */
+  @inline(__always)
+  public func sizeFor(key: PropertyKey, defaultValue: Size = Size(WorkspaceSizeZero)) -> Size {
+    return _sizes[key] ?? setSize(defaultValue, forKey: key)!
+  }
+
+  /**
+   Returns the `viewSize` of the `Size` value that is mapped to a specific `PropertyKey`.
+
+   - Parameter key: The `PropertyKey` (e.g. `LayoutConfig.FieldColourButtonSize`)
+   - Parameter defaultValue: [Optional] If no `Size` was found for `key`, this value is
+   automatically assigned to `key` and used instead.
+   - Returns: The `viewSize` of the mapped `Size` value.
+   */
+  @inline(__always)
+  public func viewSizeFor(key: PropertyKey, defaultValue: Size = Size(WorkspaceSizeZero)) -> CGSize
+  {
+    return sizeFor(key).viewSize
+  }
+
+  /**
+   Returns the `workspaceSize` of the `Size` value that is mapped to a specific `PropertyKey`.
+
+   - Parameter key: The `PropertyKey` (e.g. `LayoutConfig.FieldColourButtonSize`)
+   - Parameter defaultValue: [Optional] If no `Size` was found for `key`, this value is
+   automatically assigned to `key` and used instead.
+   - Returns: The `workspaceSize` of the mapped `Size` value.
+   */
+  @inline(__always)
+  public func workspaceSizeFor(key: PropertyKey, defaultValue: Size = Size(WorkspaceSizeZero))
+    -> WorkspaceSize
+  {
+    return sizeFor(key).workspaceSize
+  }
+
+  /**
+   Maps a `UIColor` value to a specific `PropertyKey`.
+
+   - Parameter color: The `UIColor` value
+   - Parameter key: The `PropertyKey` (e.g. `LayoutConfig.FieldCheckboxSwitchOnTintColor`)
+   - Returns: The `color` that was set.
+   */
+  public func setColor(color: UIColor?, forKey key: PropertyKey) -> UIColor? {
+    _colors[key] = color
+    return color
+  }
+
+  /**
+   Returns the `UIColor` value that is mapped to a specific `PropertyKey`.
+
+   - Parameter key: The `PropertyKey` (e.g. `LayoutConfig.FieldCheckboxSwitchOnTintColor`)
+   - Parameter defaultValue: [Optional] If no `Size` was found for `key`, this value is
+   automatically assigned to `key` and used instead.
+   - Returns: The `key`'s value
+   */
+  @inline(__always)
+  public func colorFor(key: PropertyKey, defaultValue: UIColor? = nil) -> UIColor? {
+    return _colors[key] ?? (defaultValue != nil ? setColor(defaultValue, forKey: key) : nil)
+  }
+
+  /**
+   Updates the UIView coordinate system values for all config values that have been stored
+   in this instance, by using a given `LayoutEngine`.
+
+   - Parameter engine: The `LayoutEngine` used to update all config values
+   */
+  public func updateViewValuesFromEngine(engine: LayoutEngine) {
+    for (_, var unit) in _units {
+      unit.viewUnit = engine.viewUnitFromWorkspaceUnit(unit.workspaceUnit)
+    }
+
+    for (_, var size) in _sizes {
+      size.viewSize = engine.viewSizeFromWorkspaceSize(size.workspaceSize)
+    }
+  }
+}
--- Blockly/Code/UI/Views/Default/DefaultBlockView+Render.swift
+++ Blockly/Code/UI/Views/Default/DefaultBlockView+Render.swift
@@ -19,29 +19,32 @@ extension DefaultBlockView {
   // MARK: - Public
 
   /**
-  Adds the path for drawing a next/previous notch.
-  ```
-  Draws: --
-           \_/
-  ```
-  - Parameter path: The Bezier path to add to.
-  - Parameter drawLeftToRight: True if the path should be drawn from left-to-right. False if it
-  should be drawn right-to-left.
-  - Parameter config: The `LayoutEngine.Config` to use to draw this path
-  */
-  public func addNotchToPath(path: WorkspaceBezierPath, drawLeftToRight: Bool,
-    config: LayoutEngine.Config)
+   Adds the path for drawing a next/previous notch.
+   ```
+   Draws: --
+            \_/
+   ```
+   - Parameter path: The Bezier path to add to.
+   - Parameter drawLeftToRight: True if the path should be drawn from left-to-right. False if it
+   should be drawn right-to-left.
+   - Parameter config: The `LayoutConfig` to use to draw this path
+   - Parameter notchWidth: The width of the notch, specified as a Workspace coordinate system unit.
+   - Parameter notchHeight: The height of the notch, specified as a Workspace coordinate system
+   unit.
+   */
+  public final func addNotchToPath(path: WorkspaceBezierPath, drawLeftToRight: Bool,
+    notchWidth: CGFloat, notchHeight: CGFloat)
   {
     if drawLeftToRight {
-      path.addLineToPoint(config.notchWidth.workspaceUnit - 15, 0, relative: true)
-      path.addLineToPoint(6, config.notchHeight.workspaceUnit, relative: true)
+      path.addLineToPoint(notchWidth - 15, 0, relative: true)
+      path.addLineToPoint(6, notchHeight, relative: true)
       path.addLineToPoint(3, 0, relative: true)
-      path.addLineToPoint(6, -config.notchHeight.workspaceUnit, relative: true)
+      path.addLineToPoint(6, -notchHeight, relative: true)
     } else {
-      path.addLineToPoint(-6, config.notchHeight.workspaceUnit, relative: true)
+      path.addLineToPoint(-6, notchHeight, relative: true)
       path.addLineToPoint(-3, 0, relative: true)
-      path.addLineToPoint(-6, -config.notchHeight.workspaceUnit, relative: true)
-      path.addLineToPoint(-(config.notchWidth.workspaceUnit - 15), 0, relative: true)
+      path.addLineToPoint(-6, -notchHeight, relative: true)
+      path.addLineToPoint(-(notchWidth - 15), 0, relative: true)
     }
   }
 
@@ -57,7 +60,7 @@ extension DefaultBlockView {
   ```
   - Parameter path: The Bezier path to add to.
   */
-  public func addJaggedTeethToPath(path: WorkspaceBezierPath) {
+  public final func addJaggedTeethToPath(path: WorkspaceBezierPath) {
     path.addLineToPoint(8, 0, relative: true)
     path.addLineToPoint(0, 4, relative: true)
     path.addLineToPoint(8, 4, relative: true)
@@ -66,75 +69,78 @@ extension DefaultBlockView {
   }
 
   /**
-  Adds the path for drawing a horizontal puzzle tab.
-  ```
-  Draws:
-         |
-       /\|
-      |
-       \/|
-         |
-  ```
-  - Parameter path: The Bezier path to add to.
-  - Parameter drawTopToBottom: True if the path should be drawn from top-to-bottom. False if it
-  should be drawn bottom-to-top.
-  - Parameter config: The `LayoutEngine.Config` to use to draw this path
-  */
-  public func addPuzzleTabToPath(path: WorkspaceBezierPath, drawTopToBottom: Bool,
-    config: LayoutEngine.Config)
+   Adds the path for drawing a horizontal puzzle tab.
+   ```
+   Draws:
+          |
+        /\|
+       |
+        \/|
+          |
+   ```
+   - Parameter path: The Bezier path to add to.
+   - Parameter drawTopToBottom: True if the path should be drawn from top-to-bottom. False if it
+   should be drawn bottom-to-top.
+   - Parameter puzzleTabWidth: The width of the puzzle tab, specified as a Workspace coordinate
+   system unit.
+   - Parameter puzzleTabHeight: The height of the puzzle tab, specified as a Workspace coordinate
+   system unit.
+   */
+  public final func addPuzzleTabToPath(path: WorkspaceBezierPath, drawTopToBottom: Bool,
+    puzzleTabWidth: CGFloat, puzzleTabHeight: CGFloat)
   {
-    let tabWidth = config.puzzleTabWidth.workspaceUnit
-    let totalPuzzleTabHeight = config.puzzleTabHeight.workspaceUnit
-    let verticalLineHeight = totalPuzzleTabHeight * 0.2
-    let roundedHalfPieceHeight = totalPuzzleTabHeight * 0.3
+    let verticalLineHeight = puzzleTabHeight * 0.2
+    let roundedHalfPieceHeight = puzzleTabHeight * 0.3
 
     if drawTopToBottom {
       path.addLineToPoint(0, verticalLineHeight, relative: true)
-      path.addCurveToPoint(WorkspacePointMake(-tabWidth, roundedHalfPieceHeight),
+      path.addCurveToPoint(WorkspacePointMake(-puzzleTabWidth, roundedHalfPieceHeight),
         controlPoint1: WorkspacePointMake(0, roundedHalfPieceHeight * 1.25),
-        controlPoint2: WorkspacePointMake(-tabWidth, -roundedHalfPieceHeight),
+        controlPoint2: WorkspacePointMake(-puzzleTabWidth, -roundedHalfPieceHeight),
         relative: true)
-      path.addSmoothCurveToPoint(WorkspacePointMake(tabWidth, roundedHalfPieceHeight),
-        controlPoint2: WorkspacePointMake(tabWidth, -roundedHalfPieceHeight * 0.3125),
+      path.addSmoothCurveToPoint(WorkspacePointMake(puzzleTabWidth, roundedHalfPieceHeight),
+        controlPoint2: WorkspacePointMake(puzzleTabWidth, -roundedHalfPieceHeight * 0.3125),
         relative: true)
       path.addLineToPoint(0, verticalLineHeight, relative: true)
     } else {
       path.addLineToPoint(0, -verticalLineHeight, relative: true)
-      path.addCurveToPoint(WorkspacePointMake(-tabWidth, -roundedHalfPieceHeight),
+      path.addCurveToPoint(WorkspacePointMake(-puzzleTabWidth, -roundedHalfPieceHeight),
         controlPoint1: WorkspacePointMake(0, -roundedHalfPieceHeight * 1.25),
-        controlPoint2: WorkspacePointMake(-tabWidth, roundedHalfPieceHeight),
+        controlPoint2: WorkspacePointMake(-puzzleTabWidth, roundedHalfPieceHeight),
         relative: true)
-      path.addSmoothCurveToPoint(WorkspacePointMake(tabWidth, -roundedHalfPieceHeight),
-        controlPoint2: WorkspacePointMake(tabWidth, roundedHalfPieceHeight * 0.3125),
+      path.addSmoothCurveToPoint(WorkspacePointMake(puzzleTabWidth, -roundedHalfPieceHeight),
+        controlPoint2: WorkspacePointMake(puzzleTabWidth, roundedHalfPieceHeight * 0.3125),
         relative: true)
       path.addLineToPoint(0, -verticalLineHeight, relative: true)
     }
   }
 
   /**
-  Moves the path to start drawing the top-left corner
+   Moves the path to start drawing the top-left corner
 
-  - Parameter path: The Bezier path.
-  - Parameter config: The `LayoutEngine.Config` to use to draw this path
-  */
-  public func movePathToTopLeftCornerStart(path: WorkspaceBezierPath, config: LayoutEngine.Config) {
-    path.moveToPoint(0, config.blockCornerRadius.workspaceUnit, relative: true)
+   - Parameter path: The Bezier path.
+   - Parameter blockCornerRadius: The block's corner radius, specified as a Workspace coordinate
+    system unit.
+   */
+  public final func movePathToTopLeftCornerStart(
+    path: WorkspaceBezierPath, blockCornerRadius: CGFloat)
+  {
+    path.moveToPoint(0, blockCornerRadius, relative: true)
   }
 
   /**
-  Adds the path for drawing the rounded top-left corner.
-  ```
-  Draws:   --
-          /
-         |
-  ```
-  - Parameter config: The `LayoutEngine.Config` to use to draw this path
-  */
-  public func addTopLeftCornerToPath(path: WorkspaceBezierPath, config: LayoutEngine.Config) {
-    let cornerRadius = config.blockCornerRadius.workspaceUnit
-
-    path.addArcWithCenter(WorkspacePointMake(cornerRadius, 0),
-      radius: cornerRadius, startAngle: CGFloat(M_PI), endAngle: CGFloat(M_PI * 1.5),
+   Adds the path for drawing the rounded top-left corner.
+   ```
+   Draws:   --
+           /
+          |
+   ```
+   - Parameter blockCornerRadius: The block's corner radius, specified as a Workspace coordinate
+   system unit.
+   */
+  public final func addTopLeftCornerToPath(path: WorkspaceBezierPath, blockCornerRadius: CGFloat) {
+    path.addArcWithCenter(WorkspacePointMake(blockCornerRadius, 0),
+      radius: blockCornerRadius, startAngle: CGFloat(M_PI), endAngle: CGFloat(M_PI * 1.5),
       clockwise: true, relative: true)
   }
 }
