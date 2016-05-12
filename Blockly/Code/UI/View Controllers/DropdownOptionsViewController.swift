76988ed57fea1119dd07a8cfdf0157b81c0a4977
Victor Ng <vicng@google.com>
2016-05-12 16:52:44 -0700

    Refactored FieldDropdownView into DropdownView, and FieldDropdownOptionsViewController into DropdownOptionsViewController

--- Blockly/Code/UI/View Controllers/DropdownOptionsViewController.swift
+++ Blockly/Code/UI/View Controllers/DropdownOptionsViewController.swift
@@ -16,57 +16,69 @@
 import Foundation
 
 /**
- Delegate for events that occur on `FieldDropdownOptionsViewController`.
+ Delegate for events that occur on `DropdownOptionsViewController`.
  */
-public protocol FieldDropdownOptionsViewControllerDelegate: class {
+public protocol DropdownOptionsViewControllerDelegate: class {
   /**
    Event that is called when the user has selected an option for the drop-down field.
 
    - Parameter viewController: The view controller where this event occurred.
    - Parameter optionIndex: The selected option index.
    */
-  func fieldDropdownOptionsViewController(viewController: FieldDropdownOptionsViewController,
+  func dropdownOptionsViewController(viewController: DropdownOptionsViewController,
     didSelectOptionIndex optionIndex: Int)
 }
 
 /**
- View controller for selecting an option inside a `FieldDropdown`.
+ View controller for selecting an option from inside a dropdown.
  */
-@objc(BKYFieldDropdownOptionsViewController)
-public class FieldDropdownOptionsViewController: UITableViewController {
+@objc(BKYDropdownOptionsViewController)
+public class DropdownOptionsViewController: UITableViewController {
+
+  // MARK: - Type Alias - Option
+
+  /// Represents a dropdown option, with a display name and value
+  public typealias Option = (displayName: String, value: String)
 
   // MARK: - Properties
 
-  /// The drop-down field to display
-  public var field: FieldDropdown? {
+  /// The list of drop-down options to display.
+  public var options = [Option]() {
     didSet {
       // Recalculate the preferred content size
       calculatePreferredContentSize()
     }
   }
+  /// The currently selected index.
+  public var selectedIndex = -1 {
+    didSet {
+      if selectedIndex != oldValue {
+        calculatePreferredContentSize()
+      }
+    }
+  }
   /// Delegate for events that occur on this controller
-  public weak var delegate: FieldDropdownOptionsViewControllerDelegate?
-  // TODO: (#335) Pull font style up into configuration.
+  public weak var delegate: DropdownOptionsViewControllerDelegate?
   /// The font to render each cell
   public var textLabelFont = UIFont.systemFontOfSize(18)
-  /// The maximum size to use when displaying to view controller as a popover
-  public var maximumPopoverSize = CGSizeMake(250, 250)
+  /// The maximum size to use when displaying this view controller as a popover
+  public var maximumPopoverSize = CGSizeMake(248, 248)
   /// Identifier for reusing cells for this table
-  private let cellReuseIdentifier = "FieldDropdownOptionsViewControllerCell"
+  private let cellReuseIdentifier = "DropdownOptionsViewControllerCell"
   /// The estimated width of the checkmark accessory (this value does not appear to be accessible)
   private let accessoryWidth = CGFloat(30)
   /// The cell padding
-  private let cellPadding = UIEdgeInsetsMake(15, 15, 15, 15)
+  private let cellPadding = UIEdgeInsetsMake(16, 16, 16, 16)
 
   // MARK: - Super
 
   public override func viewDidLoad() {
-    tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellReuseIdentifier)
+    tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
     tableView.reloadData()
   }
 
   public override func viewWillAppear(animated: Bool) {
-    if let selectedIndex = field?.selectedIndex {
+    if 0 <= selectedIndex && selectedIndex < options.count {
       // Automatically scroll to the selected index when the view first appears
       let path = NSIndexPath(forRow: selectedIndex, inSection: 0)
       tableView.scrollToRowAtIndexPath(path, atScrollPosition: .Middle, animated: false)
@@ -81,25 +93,25 @@ public class FieldDropdownOptionsViewController: UITableViewController {
 
   public override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
   {
-    return field?.options.count ?? 0
+    return options.count ?? 0
   }
 
   public override func tableView(
     tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
   {
-    return cellSizeForRow(indexPath.row, constrainedToWidth: self.view.bounds.size.width).height
+    return cellSizeForRow(indexPath.row, constrainedToWidth: view.bounds.width).height
   }
 
   public override func tableView(
     tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
   {
-    let cell = tableView.dequeueReusableCellWithIdentifier(self.cellReuseIdentifier,
-      forIndexPath: indexPath)
-    cell.accessoryType  = field?.selectedIndex == indexPath.row ? .Checkmark : .None
+    let cell = tableView.dequeueReusableCellWithIdentifier(
+      self.cellReuseIdentifier, forIndexPath: indexPath)
+    cell.accessoryType = (selectedIndex == indexPath.row ? .Checkmark : .None)
     cell.selectionStyle = .Default
-    cell.textLabel?.font = self.textLabelFont
+    cell.textLabel?.font = textLabelFont
     cell.textLabel?.numberOfLines = 0
-    cell.textLabel?.text = field?.options[indexPath.row].displayName
+    cell.textLabel?.text = options[indexPath.row].displayName
     cell.textLabel?.sizeToFit()
 
     return cell
@@ -112,7 +124,7 @@ public class FieldDropdownOptionsViewController: UITableViewController {
   {
     tableView.deselectRowAtIndexPath(indexPath, animated: true)
 
-    delegate?.fieldDropdownOptionsViewController(self, didSelectOptionIndex: indexPath.row)
+    delegate?.dropdownOptionsViewController(self, didSelectOptionIndex: indexPath.row)
   }
 
   // MARK: - Private
@@ -120,28 +132,23 @@ public class FieldDropdownOptionsViewController: UITableViewController {
   private func calculatePreferredContentSize() {
     var preferredContentSize = CGSizeZero
 
-    if let field = self.field {
-      for i in 0 ..< field.options.count {
-        let cellSize = cellSizeForRow(i, constrainedToWidth: maximumPopoverSize.width)
-        preferredContentSize.height =
-          min(preferredContentSize.height + cellSize.height, maximumPopoverSize.height)
-        preferredContentSize.width = max(preferredContentSize.width, cellSize.width)
-      }
+    for i in 0 ..< options.count {
+      let cellSize = cellSizeForRow(i, constrainedToWidth: maximumPopoverSize.width)
+      preferredContentSize.height =
+        min(preferredContentSize.height + cellSize.height, maximumPopoverSize.height)
+      preferredContentSize.width = max(preferredContentSize.width, cellSize.width)
     }
 
     self.preferredContentSize = preferredContentSize
   }
 
   private func cellSizeForRow(row: Int, constrainedToWidth width: CGFloat) -> CGSize {
-    guard let text = field?.options[row].displayName else {
-      return CGSizeZero
-    }
-
     // Measure the text width (taking into account the cell's x-padding)
+    let text = options[row].displayName
     let xPadding =
-      cellPadding.left + cellPadding.right + (field?.selectedIndex == row ? accessoryWidth : 0)
+      cellPadding.left + cellPadding.right + (selectedIndex == row ? accessoryWidth : 0)
     let width = width - xPadding
-    var size = text.bky_multiLineSizeForFont(self.textLabelFont, constrainedToWidth: width)
+    var size = text.bky_multiLineSizeForFont(textLabelFont, constrainedToWidth: width)
 
     // Add the cell padding back to the measured text size
     size.height += cellPadding.top + cellPadding.bottom
