5fe609d253b77831b113b161be3029fbcd8188b7
Victor Ng <vicng@google.com>
2017-04-19 17:06:26 -0700

    More codelab cleanup

--- Samples/BlocklyCodeLab/BlocklyCodeLab/Resources/Non-Localized/sound_block_generators.js
+++ Samples/BlocklyCodeLab/BlocklyCodeLab/Resources/Non-Localized/sound_block_generators.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// This is defined to automatically throw a JS exception if an infinite loop is detected.
-//Blockly.JavaScript.INFINITE_LOOP_TRAP = 'if(--window.LoopTrap == 0) throw "Infinite loop.";\n';
-
 // Generators for blocks defined in `sound_blocks.json`.
 Blockly.JavaScript['play_sound'] = function(block) {
   var value = '\'' + block.getFieldValue('VALUE') + '\'';
